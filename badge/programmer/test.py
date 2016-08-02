150a01d6c403a0c0e45d71eae63a5a3b8e8031b3
Alvaro Prieto <apg88zx@gmail.com>
2016-08-02 11:17:16 -0700

    update test script

--- badge/programmer/test.py
+++ badge/programmer/test.py
@@ -12,15 +12,21 @@ if flasher.connected is True:
     flasher._sendCmd('reset halt')
 
     # Read device unique ID
-    uid = flasher.readMem(0x1FFFF7E8, 12)
+    uid_bytes = flasher.readMem(0x1FFFF7E8, 12)
+    uid = ''
+    for byte in range(len(uid_bytes)):
+    	uid += '{:02X}'.format(uid_bytes[byte])
     print('uid:')
     print(uid)
 
     flasher.erase(0x800d400, 0x400)
 
-    flasher.flashFile('/Users/alvaro/Desktop/test.bin', 0xd400)
+    flasher.flashFile('/Users/alvaro/Desktop/test.bin', 0x800d400)
 
-    flasher.dumpImage('/Users/alvaro/Desktop/flash.bin', 0x800d400, 0x400)
+    print flasher.verifyFile('/Users/alvaro/Desktop/test.bin', 0x800d400)
+    print flasher.verifyFile('/Users/alvaro/Desktop/flash.bin', 0x800d400)
+
+    # flasher.dumpImage('/Users/alvaro/Desktop/flash.bin', 0x800d400, 0x400)
 
 flasher.kill()
 
