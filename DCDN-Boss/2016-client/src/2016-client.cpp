26e228f0fdb410e9ea77fd5e2ae460e9a9b6d098
cmdc0de <cmdc0dez@gmail.com>
2016-08-06 02:29:17 -0700

    boss hd program done

--- DCDN-Boss/2016-client/src/2016-client.cpp
+++ DCDN-Boss/2016-client/src/2016-client.cpp
@@ -1,72 +1,64 @@
-//============================================================================
-// Name        : 2016-client.cpp
-// Author      : CmdC0de
-// Version     :
-// Copyright   : DCDarkNet Industries LLC  all right reserved
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-#include <netinet/in.h>
 #include <sys/socket.h>
-#include <sys/wait.h>
-#include <arpa/inet.h>
-#include <fcntl.h> /* Added for the nonblocking socket */
-#include <time.h>
-#include <list>
-#include <unistd.h>
-using namespace std;
+#include <netinet/in.h>
+#include <netdb.h>
 
-const char *SERVER = "127.0.0.1";
-const unsigned short MYPORT = 3456;
+void error(const char *msg) {
+	perror(msg);
+	exit(0);
+}
 
-int main() {
+bool readAllData(int fd) {
+	char buffer[256];
+	bzero(buffer, 256);
+	int n = read(fd, buffer, sizeof(buffer));
+	if(n==0) return false;
+	printf("%s\n",buffer);
+	return true;
+}
 
-	char inPut[128];
-	int sockfd = 0, new_fd = 0; /* listen on sock_fd, new connection on new_fd */
-	struct sockaddr_in my_addr; /* my address information */
-	struct sockaddr_in their_addr; /* connector's address information */
-	unsigned int sin_size;
-	char string_read[256];
-	memset(&string_read[0], 0, sizeof(string_read));
+int main(int argc, char *argv[]) {
+	int sockfd, portno, n;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
 
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		perror("socket");
-		exit(1);
+	char buffer[256];
+	portno = 3456;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+		error("ERROR opening socket");
+	server = gethostbyname("localhost");
+	if (server == NULL) {
+		fprintf(stderr, "ERROR, no such host\n");
+		exit(0);
+	}
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *) server->h_addr,
+	(char *)&serv_addr.sin_addr.s_addr,
+	server->h_length);
+	serv_addr.sin_port = htons(portno);
+	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		error("ERROR connecting");
+	}
+	n = write(sockfd, "MONA", 4);
+	usleep(100);
+	readAllData(sockfd);
+
+	for(int i=0;i<6;i++) {
+		bzero(buffer, 256);
+		fgets(buffer, 255, stdin);
+		n = write(sockfd, buffer, strlen(buffer));
+		if(n<=0) break;
+		usleep(1000);
+		if(!readAllData(sockfd)) break;
 	}
 
-	my_addr.sin_family = AF_INET; /* host byte order */
-	my_addr.sin_port = htons(MYPORT); /* short, network byte order */
-	my_addr.sin_addr.s_addr = inet_addr(SERVER);
-	bzero(&(my_addr.sin_zero), 8); /* zero the rest of the struct */
-
-	int fd = connect(sockfd, (struct sockaddr*) &my_addr, sizeof(my_addr));
-
-	int ret = 0;
-	if (fd > 0) {
-		if ((ret = send(fd, "MONA", 4, 0)) != -1) {
-			if ((ret = recv(fd, &string_read[0], sizeof(string_read), 0)) != -1) {
-				cout << string_read << endl;
-			}
-		}
-		while (ret != -1) {
-			cin.getline(&inPut[0], sizeof(inPut));
-			ret = send(fd, &inPut[0], strlen(&inPut[0]),0);
-			if (ret != -1) {
-				memset(&string_read[0], 0, sizeof(string_read));
-				if ((ret = recv(fd, &string_read[0], sizeof(string_read), 0)) != -1) {
-					cout << string_read << endl;
-				}
-			}
-		}
-	} else {
-		cout << "failed to connect" << endl;
-	}
-
+	close(sockfd);
 	return 0;
 }
+
--- DCDN-Boss/2016/main.cpp
+++ DCDN-Boss/2016/main.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <string.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
@@ -11,49 +10,48 @@
 #include <time.h>
 #include <list>
 #include <unistd.h>
+#include <string>
+#include <cstring>
 
 #define MYPORT 3456    /* the port users will be connecting to */
 #define BACKLOG 10     /* how many pending connections queue will hold */
 
-static const int MAX_TIME_BETWEEN_DATA = 45;
+static const int MAX_TIME_BETWEEN_DATA = 120;
 static const int MAX_TIME_FOR_CONNECTION = MAX_TIME_BETWEEN_DATA * 4;
 
-static const char *CYBEREZ[3][16] = {
-		"Cyberez Inc.",
-		"Cyb3r3z 1nc.",
-		"cYber3z 11c"
-};
+static const char *CYBEREZ[3][16] = { "Cyberez Inc.", "Cyb3r3z 1nc.", "cYber3z 11c" };
 
 static const char *HEX_STRING = "0123456789ABCDEF";
 
 void generateRandomShit(char *p, unsigned int n) {
-	bool addCyberez = ((rand()%100)<10?true:false);
+	bool addCyberez = ((rand() % 100) < 15 ? true : false);
 	int where = -1;
-	if(addCyberez) {
-		int loc = rand()%3;
-		switch(loc) {
+	if (addCyberez) {
+		int loc = rand() % 3;
+		switch (loc) {
 		case 1:
 			where = 0;
 			break;
 		case 2:
-			where = n/2;
+			where = n / 2;
 			break;
 		case 3:
-			where = n-16;
+			where = n - 16;
 			break;
 		default:
 			break;
 		}
 	}
-	for(unsigned int i=0;i<n;i++) {
-		if(i==where) {
-			int which = rand()%3;
-			strcpy(&p[i],CYBEREZ[which][0]);
-			i+=strlen(CYBEREZ[which][0]);
+	for (unsigned int i = 0; i < n - 1; i++) {
+		if (i == where) {
+			int which = rand() % 3;
+			strncpy(&p[i], CYBEREZ[which][0],strlen(CYBEREZ[which][0]));
+			i += strlen(CYBEREZ[which][0]);
 		} else {
-			p[i] =HEX_STRING[rand()%17];
+			p[i] = HEX_STRING[rand() % 16];
 		}
 	}
+	p[n] = '\0';
 }
 
 struct ClientInfo {
@@ -61,8 +59,38 @@ struct ClientInfo {
 	int RightAnswers;
 	time_t ConnectTime;
 	time_t LastDataReceived;
+	std::string InputBuffer;
+	std::string OutputBuffer;
+	bool Dead;
+	void bufferIn() {
+		char buf[256];
+		int n = recv(FD, &buf[0], sizeof(buf), 0);
+		if (n > 0) {
+			InputBuffer.append(&buf[0], n);
+			LastDataReceived = time(0);
+		} else {
+			if (n == 0 || (n < 0 && errno != EAGAIN)) {
+				Dead = true;
+			}
+		}
+	}
+	void bufferOut(char *b, int n) {
+		OutputBuffer.append(b, n);
+	}
+	void sendAll() {
+		if(OutputBuffer.length()>0) {
+			int n = 0;
+			while (OutputBuffer.length() > 0 && (n = send(FD, OutputBuffer.data(), OutputBuffer.length(), 0)) > 0) {
+				OutputBuffer = OutputBuffer.substr(n);
+			}
+			if (n == 0 || (n == -1 && errno != EAGAIN)) {
+				Dead = true;
+			}
+		}
+	}
 	ClientInfo(int fd) :
-			FD(fd), RightAnswers(0), ConnectTime(time(0)), LastDataReceived(time(0)) {
+			FD(fd), RightAnswers(0), ConnectTime(time(0)), LastDataReceived(time(0)), InputBuffer(), OutputBuffer(), Dead(
+					false) {
 	}
 	~ClientInfo() {
 		close(FD);
@@ -75,7 +103,6 @@ int main(int arc, char *agrv[]) {
 	struct sockaddr_in my_addr; /* my address information */
 	struct sockaddr_in their_addr; /* connector's address information */
 	unsigned int sin_size;
-	char string_read[64];
 
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
 		perror("socket");
@@ -88,6 +115,8 @@ int main(int arc, char *agrv[]) {
 	bzero(&(my_addr.sin_zero), 8); /* zero the rest of the struct */
 
 	fcntl(sockfd, F_SETFL, O_NONBLOCK); /* Change the socket into non-blocking state	*/
+	int optval = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
 	if (bind(sockfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr)) == -1) {
 		perror("bind");
 		exit(1);
@@ -104,55 +133,64 @@ int main(int arc, char *agrv[]) {
 	CLIENT_LIST ListOfSockets;
 
 	char Results[7][20] = { "MONA", "XfjnhD0ZQ8", "5zQXLfSo71", "E2ElmnWDuv", "MY8VBVunA6", "ZWxEcrPWc0", "4OmUw7DuEo" };
+	char Prompt[7][20] = { "#connection\n", "#datadown\n", "#dataup\n", "#keygen\n", "#10/6\n", "#initiate\n" };
 
 	bool keepRunning = true;
 	while (keepRunning) {
 		sin_size = sizeof(struct sockaddr_in);
 		if ((new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &sin_size)) != -1) {
+			fcntl(new_fd, F_SETFL, O_NONBLOCK);
 			printf("server: got connection from %s\n", inet_ntoa(their_addr.sin_addr));
 			ListOfSockets.push_back(new ClientInfo(new_fd));
 		}
 		std::list<CLIENT_LIST_IT> removeList;
 		CLIENT_LIST_IT it;
 		for (it = ListOfSockets.begin(); it != ListOfSockets.end(); ++it) {
-			int n = recv(new_fd, string_read, sizeof(string_read), 0);
-			if (n == 0) {
-				removeList.push_back(it);
-			} else if (n > 0) {
-				(*it)->LastDataReceived = time(0);
-				if (strcmp(&Results[(*it)->RightAnswers][0], string_read) == 0) {
-					if ((*it)->RightAnswers == 6) {
-						static const char *success = "March Hare daemon initialized";
-						send((*it)->FD, success, strlen(success), 0);
-						keepRunning = false;
+			if (!(*it)->Dead) {
+				(*it)->bufferIn();
+
+				if ((*it)->InputBuffer.length() > 1) {
+					if (strncmp(Results[(*it)->RightAnswers], (*it)->InputBuffer.data(),
+							strlen(Results[(*it)->RightAnswers])) == 0) {
+						(*it)->InputBuffer.clear();
+						if ((*it)->RightAnswers == 6) {
+							static const char *success = "March Hare daemon initialized";
+							send((*it)->FD, success, strlen(success), 0);
+							keepRunning = false;
+						} else {
+							//send((*it)->FD, Prompt[(*it)->RightAnswers], strlen(Prompt[(*it)->RightAnswers]), 0);
+							(*it)->bufferOut(Prompt[(*it)->RightAnswers], strlen(Prompt[(*it)->RightAnswers]));
+							(*it)->RightAnswers++;
+							char buf[128];
+							generateRandomShit(&buf[0], sizeof(buf));
+							(*it)->bufferOut(buf,128);
+						}
 					} else {
-						(*it)->RightAnswers++;
-						char buf[128];
-						generateRandomShit(&buf[0], sizeof(buf));
-						//send random hex string with a chance of having cyberez in it
-						send((*it)->FD,&buf[0],sizeof(buf),0);
+						printf("Wrong answer sent by connection: %s", inet_ntoa(their_addr.sin_addr));
+						(*it)->Dead = true;
 					}
 				} else {
-					removeList.push_back(it);
+					if (time(0) - (*it)->LastDataReceived > MAX_TIME_BETWEEN_DATA) {
+						printf("%s too much time between data", inet_ntoa(their_addr.sin_addr));
+						(*it)->Dead = true;
+					}
+					if (time(0) - (*it)->ConnectTime > MAX_TIME_FOR_CONNECTION) {
+						printf("%s was connected for too long", inet_ntoa(their_addr.sin_addr));
+						(*it)->Dead = true;
+					}
 				}
+				(*it)->sendAll();
 			} else {
-				if (time(0) - (*it)->LastDataReceived > MAX_TIME_BETWEEN_DATA) {
-					removeList.push_back(it);
-				}
-				if (time(0) - (*it)->ConnectTime > MAX_TIME_FOR_CONNECTION) {
-					removeList.push_back(it);
-				}
+				removeList.push_back(it);
 			}
 		}
 		//ListOfSockets.erase(removeList.begin(), removeList.end());
 		for (std::list<CLIENT_LIST_IT>::iterator rit = removeList.begin(); rit != removeList.end(); ++rit) {
 			ListOfSockets.erase(*rit);
-			delete (*it);
+			printf("dropping connection");
+			delete (*(*rit));
 		}
-		struct timespec ts;
-		ts.tv_sec = 0;
-		ts.tv_nsec = 100 * 1000000;
-		nanosleep(&ts, NULL);
+		usleep(50);
 	}
 }
 
